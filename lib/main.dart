96665cbd42da27c083607d8f9afd8c8ea249b69a
sami <samyhisham992@gmail.com>
2024-12-21 19:41:24 +0000

    add ui for market screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:el_fares/pages/add_market.dart';
 import 'package:el_fares/pages/splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
@@ -12,9 +13,9 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const GetMaterialApp(
+    return GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      home: SplashScreenPage(),
+      home: AddMarket(),
     );
   }
 }
--- lib/pages/add_market.dart
+++ lib/pages/add_market.dart
@@ -1,10 +1,45 @@
+import 'package:el_fares/components/my_button.dart';
+import 'package:el_fares/components/my_textfield.dart';
 import 'package:flutter/material.dart';
 
 class AddMarket extends StatelessWidget {
-  const AddMarket({super.key});
+  AddMarket({super.key});
+
+  final TextEditingController _marketNameController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Add Market'),
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            SizedBox(
+              height: 200,
+            ),
+            MyTextfield(
+                hintText: 'Enter Name Market',
+                obscure: false,
+                controller: _marketNameController),
+            SizedBox(
+              height: 50,
+            ),
+            MyButton(
+              onPressed: () => {},
+              text: 'N E X T',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 16,
+              ),
+              backgroundColor: Colors.blueAccent.shade400,
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
